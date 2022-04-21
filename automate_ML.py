7f8b84471336595d861e40be7f4fa56a734421aa
Kaushal J Kanani <76743140+Kaushal-developer@users.noreply.github.com>
2022-04-22 03:16:30 +0530

    make it better

--- automate_ML.py
+++ automate_ML.py
@@ -1,5 +1,8 @@
 import csv
+from os import remove
+import queue
 from nsetools import Nse
+from numpy import append
 from tradingview_ta import TA_Handler, Interval, Exchange
 
 choices = {'STRONG_BUY':'STRONG BUY', 'STRONG_SELL': 'STRONG SELL','BUY': 'BUY', 'SELL':'SELL','NEUTRAL':'NEUTRAL'}
@@ -23,67 +26,66 @@ for code, name in nse_stock_codes.items():
 
 
 class AutoML:
-    def check_stock_is_in_bse(self,stock_name):
-        matched_key =[]
-        for key in bse_stock_list:
-            if stock_name in key:
-                matched_key.append(bse_stock_list[key])
+    def check_stock(self,stock_name,exchange):
+        if exchange == 'BSE':
+            matched_key =[]
+            for key in bse_stock_list:
+                if stock_name in key:
+                    matched_key.append(bse_stock_list[key])
 
-        return matched_key
+            return matched_key
+        else:
+            matched_key =[]
+            for key in nse_stock_list:
+                if stock_name in key:
+                    matched_key.append(nse_stock_list[key])
 
-    def check_stock_is_in_nse(self,stock_name):
-        matched_key =[]
-        for key in nse_stock_list:
-            if stock_name in key:
-                matched_key.append(nse_stock_list[key])
-
-        return matched_key
+            return matched_key
         
-    def get_stock_prediction(self,stock_name):
+    def get_stock_prediction(self,stock_name,exchange):
         
-        nse_key_list = self.check_stock_is_in_nse(stock_name)
-        bse_key_list = self.check_stock_is_in_bse(stock_name)
-        recommandations_NSE = {}
-        for code in nse_key_list:
+        key_list = self.check_stock(stock_name,exchange)
+        recommandations = {}
+       
+        for code in key_list:
             scrip = TA_Handler(
                 symbol=code,
                 screener="india",
-                exchange="NSE",
+                exchange=exchange,
                 interval=Interval.INTERVAL_1_DAY,
-                # proxies={'http': 'http://example.com:8080'} # Uncomment to enable proxy (replace the URL).
+                proxies={'http': 'http://example.com:8080'} # Uncomment to enable proxy (replace the URL).
             )
-            if scrip.get_analysis():
-                print(scrip.get_analysis().summary)
-                recommandations_NSE[code] = scrip.get_analysis().summary["RECOMMENDATION"]
+            try:
+                if scrip and scrip.get_analysis() and scrip.get_analysis().summary:
+                    recommandations[code] = scrip.get_analysis().summary["RECOMMENDATION"]
+            except Exception:
+                continue
+        return recommandations
 
-        recommandations_BSE = {}
-        for code in bse_key_list:
-            scrip = TA_Handler(
-                symbol=code,
-                screener="india",
-                exchange="BSE",
-                interval=Interval.INTERVAL_1_DAY,
-                # proxies={'http': 'http://example.com:8080'} # Uncomment to enable proxy (replace the URL).
-            )
-            if scrip.get_analysis():
-                recommandations_BSE[code] = scrip.get_analysis().summary["RECOMMENDATION"]
-        return recommandations_NSE, recommandations_BSE
-
-    def get_return_message(self,stock_name):
-        nse_result,bse_result = self.get_stock_prediction(stock_name)
-        text = ''
-        for code,result in nse_result.items():
+    def get_return_message(self,stock_name,exchange):
+        result = self.get_stock_prediction(stock_name,exchange)
+        text = []
+        for code,result in result.items():
             if result in choices:
-                text += "NSE based Company: " + nse_stock_codes[code] +' '+ choices[result] + ' would be preferrable.'
-                text += "                   "
-        for code,result in bse_result.items():
-            if result in choices:
-                text += "BSE based Company: " + bse_stock_code[code] +' '+ choices[result] + ' would be preferrable.'
-                text += "        "
-        if text == "":
-            text = "Please try with company full name with underscore pattern like /tata_power."
+                text.append(exchange + " based Company: " + nse_stock_codes[code] +' \n'+ choices[result] + ' would be preferrable.')
+        if len(text) == 0:
+            text.append("Please try with company full name with underscore pattern like /tata_power.")
 
-        if nse_result == {} and bse_result == {}:
-            text = ""
-            text += 'Mr. BiLLA have not enough data to calculate predictions for the '+ stock_name +'. Please Check regularly to get predictions.'
+        if result == {}:
+            text = []
+            text.append('Mr. BiLLA have not enough data to calculate predictions for the '+ stock_name +'. Please Check regularly to get predictions.')
         return text
+
+
+class Queue:
+    def __init__(self,queue_list):
+        self.queue_list = queue_list
+
+    def insert(self,data):
+        self.queue_list.append(data)
+
+    def remove(self,data):
+        del self.queue_list[0]
+
+    def get(self):
+        return self.queue_list
--- automate_telegram.py
+++ automate_telegram.py
@@ -1,15 +1,15 @@
 
-from telegram.ext import Updater, MessageHandler, CommandHandler, Filters
+from gc import callbacks
+from telegram.ext import Updater, MessageHandler, CommandHandler, Filters,CallbackQueryHandler
 from tradingview_ta import TA_Handler, Interval, Exchange
-import requests
-from nsetools import Nse
-import re
+import telegram
 import time
-from automate_ML import AutoML
+from automate_ML import AutoML,Queue
 from decouple import config
 import os
 
 ml = AutoML()
+queue = Queue([])
 API_KEY = config('KEY')
 
 PORT = int(os.environ.get('PORT', '8443'))
@@ -25,16 +25,38 @@ def parse_msg(update, context):
     if update is None:
         context.bot.send_message(chat_id='@channelid', text='this is a test')    
     if '/' in update["message"]["text"]:
-        chat_id = update["message"]["chat"]["id"]
-        user_messaged = update["message"]["text"].replace('/','').lower()
-        message = ml.get_return_message(user_messaged)
-        context.bot.send_message(text=message,chat_id=chat_id)
-        time.sleep(1)
+        keyboard = [
+            [
+                telegram.InlineKeyboardButton("NSE",callback_data='NSE'),
+                telegram.InlineKeyboardButton("BSE", callback_data='BSE'),
+            ]
+        ]
+        queue.insert({"chat_id":update["message"]["chat"]["id"],"text":update["message"]["text"].replace('/','').lower()})
+        reply_markup = telegram.InlineKeyboardMarkup(keyboard)        
+        message = 'Please choose the stock exchange....\n' + 'NSE - National stock exchange\n' + 'BSE - Bombay stock exchange\n'
+        update.message.reply_text(message, reply_markup=reply_markup)
+
+
+def call_back_button(update, context):
+    selection = update["callback_query"]["data"] if update["callback_query"]["data"] else ""
+    previouse_chat = queue.get()
+    current_queue = previouse_chat[0] if len(previouse_chat) > 0 else None
+    if current_queue:
+        user_messaged = current_queue["text"] 
+        messages = ml.get_return_message(user_messaged,selection)
+        for msg in messages:
+            queue.remove(previouse_chat[0])
+            time.sleep(0.5)
+            context.bot.send_message(text=msg,chat_id=current_queue["chat_id"] )
+            
+
 
 def main():
     updater = Updater(API_KEY, use_context=True)
     dp = updater.dispatcher
     dp.add_handler(MessageHandler(Filters.text, parse_msg))    
+    dp.add_handler(CallbackQueryHandler(call_back_button, 
+        pattern='^(NSE|BSE)$'))
     # updater.start_polling()
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
